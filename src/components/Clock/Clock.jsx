70895388285cddc5cbe556c8efc69bfd71365a10
acreemos <acreemos@gmail.com>
2021-12-14 16:49:19 +0300

    feat: add clock block

--- src/components/Clock/Clock.jsx
+++ src/components/Clock/Clock.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Clock = () => {
+const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString())
+const [currentDate, setCurrentDate] = useState(new Date().toLocaleDateString())
+
+setInterval(() => {
+  setCurrentTime(new Date().toLocaleTimeString('ru-RU'))
+  setCurrentDate(new Date().toLocaleDateString('ru-RU', {weekday: 'long', month: 'long', day: 'numeric'}))
+}, 1000)
+
+
   return (
     <>
-      <time class="time"></time>
-      <date class="date"></date>
+      <time class="time">{currentTime}</time>
+      <date class="date">{currentDate}</date>
     </>
   );
 };
